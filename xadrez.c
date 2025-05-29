7eabdc19359ef0cc564f8f44597dd198cd56da2a
Lara Costa <101888798+costalr@users.noreply.github.com>
2025-05-29 19:52:21 +0000

    Desafio xadrez nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,49 @@
 #include <stdio.h>
 
 
-void moverTorre() {
-    int i = 0;
-    while(i<5) {
+void moverTorre(int i) {
+    if (i<5) {
         printf("Torre - Direita\n");
-        i++;
+        moverTorre(i+1);
     }
 }
 
-void moverBispo() {
-    int i = 0;
-    do {
-        printf("Bispo - Cima, Direita\n");
-        i++;
-    } while (i<5);
+void moverRainha(int i) {
+    if (i<8) {
+    printf("Rainha - Esquerda\n");
+    moverRainha(i+1);
+}
 }
 
-void moverRainha() {
-    for (int i = 0; i <8; i++) {
-        printf("Rainha - Esquerda\n");
-        
+void moverBispo(int i) {
+    if (i < 5) {
+        printf("Bispo - Cima\n");
+        for (int j = 0; j < 1; j++) {
+            printf("Bispo - Direita\n");
+        }
+        moverBispo(i + 1);
     }
 }
 
 void moverCavalo() {
         for (int i = 0; i < 2; i++) {        
-        printf("Baixo\n");
+        printf("Cavalo - Baixo\n");
     
         int j = 0;
         while (j < 1 && i == 1) {        
-            printf("Esquerda\n");
+            printf("Cavalo - Esquerda\n");
             j++;
         }
 }
 }
 
 int main() {
-    moverTorre();
-    moverBispo();
-    moverRainha(); 
+    moverTorre(0);
+    printf("\n");
+    moverBispo(0);
+    printf("\n");
+    moverRainha(0);
+    printf("\n");
     moverCavalo();
     return 0;
 }
